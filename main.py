42caa562dfe37b402fc86b68a637159830b534ce
IgorR02 <igor.ropiak@gmail.com>
2022-03-17 13:08:55 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import math
+# Zad 1
+# a = True
+# b = 3
+# c = "ABC"
+# print(a,b,c)
+# Zad 2
+# a = 1
+# b = 3
+# print(a + b, b - a, a * b, a / b)
+# Zad 3
+# a = 2
+# b = 6
+# a+=1
+# b-=1
+# b%=2
+# a**=2
+# print(a,b)
+# Zad 4
+# print(math.e ** 10)
+# print((math.log(5 + math.pow(math.sin(8),2))) **(1/6))
+# print(math.floor(3.55),math.ceil(4.80))
+# Zad 5
+# name = "IGOR"
+# surname = "ROPIAK"
+# print(name.capitalize(),surname.capitalize())
+# Zad 6
+# lyrics = "La la, la la la la la na na na na na" \
+#          "La la na na, la la la la la na na na na na" \
+#          "La la, la la la la la na na na na na" \
+#          "La la na na, la la la la la na na na na na"
+# print("Ilosc La:" + str(lyrics.count("la") + lyrics.count("La")))
+# Zad 7
+# name = "Igor"
+# print(name[1],name[3])
+# Zad 8
+# name = "Igor Ropiak"
+# print(name.split(" "))
+# Zad 9
+# sting = "Sting"
+# flo = 4.85
+# he = 10
+# print(sting, str(flo), hex(he))
